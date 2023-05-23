8cf043bc25ff1343d09e9c174d196086abca9d38
Super-Zhang <874993952@qq.com>
2023-05-23 09:07:10 +0800

    Initial commit

---
+++ shiro_login/src/main/java/com/nuomi/shirodemo/ShiroRun.java
@@ -0,0 +1,15 @@
+package com.nuomi.shirodemo;
+
+/**
+ * @Author: zh
+ * @Date: 2023-05-23-9:03
+ * @Description: Shiro 的第一个类
+ */
+public class ShiroRun {
+    public static void main(String[] args) {
+        //1. 初始化获取SecurityManager
+        //2. 获取Subject对象
+        //3. 创建token对象，获取从前端传来的用户名密码。
+        //4. 调用对象方法完成登录
+    }
+}
